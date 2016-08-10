27737e6949559808b52737274298d1d48781e7f1
Miguel Díaz <ademouse@gmail.com>
2016-08-10 12:13:55 +0200

    cordova platform add android --save

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,10 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-crosswalk-webview": "1.8.0"
+}
+// BOTTOM OF METADATA
+});
